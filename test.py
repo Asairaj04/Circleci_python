9755228f75d781844d61e70b09e4056bc646ecb7
Asairaj04 <saipatkar04@gmail.com>
2025-08-06 14:24:59 +0530

    added circleci config updated some changes file

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ from main import to_upper
 
 class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
-        name = "Shubham"
+        name = "Sairaj Patkar"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "SHUBHAM")
+        self.assertEqual(upper_name, "SAIRAJ PATKAR")
 
 
 if __name__ == '__main__':
