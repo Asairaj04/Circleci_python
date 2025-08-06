e83412c160d7d903802fd22954b0f611e728359b
Asairaj04 <saipatkar04@gmail.com>
2025-08-06 14:32:36 +0530

    added new update job

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ from main import to_upper
 
 class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
-        name = "Sairaj Patkar"
+        name = "Sairaj"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "SAIRAJ PATKAR")
+        self.assertEqual(upper_name, "SAIRAJ")
 
 
 if __name__ == '__main__':
